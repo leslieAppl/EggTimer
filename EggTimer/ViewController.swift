1f760372af9e46333837be35c971f7806389adf1
Leslie <leslie.appl@gmail.com>
2020-05-28 21:44:47 +0800

    checked out

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,10 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var timer = Timer()
+    var time = 210
+    
+    @IBOutlet weak var timeLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    @objc func decreaseTimer() {
+        if time > 0 {
+            
+            time -= 1
+            
+            timeLbl.text = String(time)
+            
+        } else {
+            
+            timer.invalidate()
+            
+        }
+    }
+    
+    @IBAction func pauseBtnPressed(_ sender: UIBarButtonItem) {
+        
+        timer.invalidate()
+        
+    }
+    
+    @IBAction func playBtnPressed(_ sender: UIBarButtonItem) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+        
+    }
+
+    @IBAction func minusTenBtnPressed(_ sender: UIBarButtonItem) {
+        
+        if time > 10 {
+            
+            time -= 10
+            
+            timeLbl.text = String(time)
+            
+        }
+    }
+
+    @IBAction func plusTenBtnPressed(_ sender: UIBarButtonItem) {
+        
+        time += 10
+        
+        timeLbl.text = String(time)
+        
+    }
+
+    @IBAction func resetBtnPressed(_ sender: UIBarButtonItem) {
+        
+        time = 210
+        
+        timeLbl.text = String(time)
+        
     }
 
 
